2a18e49a2fc5658fa1524a627e672098ee74b4cb
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-03-21 14:02:08 +0900

    자습서: 1단계 async는 뭘까?

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
-from fastapi.responses import FileResponse
+
 
 @app.get("/")
-def read_root():
-    return FileResponse('index.html')
+async def root():
+    return {"message": "Hello World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# 1단계를 공부하고
+# fastapi는 장고랑 다르게 async 기능이 있다. 이 덕분에 nodeJS처럼 속도가 빠르다고 한다. 
+# 그렇다면 이 async는 어느 상황에 사용해야 되는 걸까? 
