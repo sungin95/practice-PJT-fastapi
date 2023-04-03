687a3a19f9e317f4801c4269f35f2df2db83726e
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-04-03 19:08:29 +0900

    MYSQL 연결 성공!!! - sql_app을 현재 경로로 인식을 못하고 있다. 구글링을 해 봤으나 잘 이해를 못하겠 다... 임시 방편으로 crud from *  이런식으로 써 주었다. - MySQL 비밀번호를 @를 넣어서 고생좀 했다. @는 구분하는데 사용하므로 절대 비밀번호에 @를 사용 하지 않도록 하자. - database.py에 URL에 "...?charset=utf8mb4" 를 넣었으면 create_engine에 또 넣지 말자 - DB정보는 secrits.json에 있다. gitignore 해 놓았다.

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,13 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import json
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_FILE = os.path.join(BASE_DIR, "secrets.json")
+secrets = json.loads(open(SECRET_FILE).read())
+DB = secrets["DB"]
+"mysql+pymysql://some_user:some_pass@some_host/test?charset=utf8mb4"
+"mysql+pymysql://root:@As5121926@127.0.0.1:3306/mydatabase?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8mb4"
+# "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    SQLALCHEMY_DATABASE_URL
+)  # connect_args={"check_same_thread": False
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
